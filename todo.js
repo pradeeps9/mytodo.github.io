b41e1cfc66a7d328d6c97c51449fbfde6511206b
Pradeep Bansiwal <46450532+pradeeps9@users.noreply.github.com>
2023-08-07 20:57:20 +0530

    my todo files

---
+++ todo.js
@@ -0,0 +1,63 @@
+let newTask = document.getElementById("addTask");
+let taskNo = 1;
+
+// Only a-z char are valid
+newTask.addEventListener("keydown", function (e){
+  if(e.key.match(/[a-zA-Z0-9 ]/g) == null){
+    e.preventDefault();
+  }
+});
+
+//* functionality to add a new task
+
+newTask.addEventListener("keydown", function (e) {
+  if(e.key == "Enter" && newTask.value != 0){
+    // console.log("Enter key is presed");
+    // console.log(newTask.value);
+    addNewTask(newTask.value);
+    newTask.value = "";
+  }
+});
+
+function addNewTask(t) {
+  let li = document.createElement("li");
+  let input = document.createElement("input");
+  input.type = "checkbox";
+  let label = document.createElement("label");
+  label.innerText = t;
+  
+  // set attributes
+  input.classList.add("task");
+  ++taskNo;
+  input.setAttribute("id", `task${taskNo}`);
+  label.setAttribute("for", `task${taskNo}`);
+
+
+  //add elements
+  li.appendChild(input);
+  li.appendChild(label);
+
+  let taskBox = document.getElementById("taskList");
+  taskBox.prepend(li);
+}
+
+
+//* Delete task functionality
+
+let checkboxes = document.getElementsByClassName("task");
+let del = document.querySelector(".del");
+
+del.addEventListener("click", function () {
+  for(let box of checkboxes){
+    if(box.checked == true){
+      let list = box.parentElement; // li's to be deleted
+      
+      setTimeout( () => {
+        list.remove();
+      }, 250);
+    }
+  }
+});
+
+
+
